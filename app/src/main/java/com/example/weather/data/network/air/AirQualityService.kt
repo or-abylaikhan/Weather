f33201aa53b28cfe5245fd3df86f06aff7216a6b
abylaikhanoralbek <abylaikhan513@gmail.com>
2022-12-18 01:34:21 +0600

    [update] retrofit response type

--- app/src/main/java/com/example/weather/data/network/air/AirQualityService.kt
+++ app/src/main/java/com/example/weather/data/network/air/AirQualityService.kt
@@ -1,9 +1,9 @@
 package com.example.weather.data.network.air
 
-import retrofit2.Response
+import retrofit2.Call
 import retrofit2.http.GET
 
 interface AirQualityService {
     @GET("v1/air-quality?hourly=pm10")
-    fun getAirQuality(lat: Double, long: Double): Response<AirQualityDto>
+    fun getAirQuality(lat: Double, long: Double): Call<AirQualityDto>
 }
